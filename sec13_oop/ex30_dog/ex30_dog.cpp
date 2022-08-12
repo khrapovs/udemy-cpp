925c1f160ba3c2e61fd1daf332d653dbe3e9172d
Stanislav Khrapov <khrapovs@gmail.com>
2022-08-12 18:12:34 +0200

    ex30

---
+++ sec13_oop/ex30_dog/ex30_dog.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Dog.h"
+
+
+Dog test_dog() {
+//---- WRITE YOUR CODE BELOW THIS LINE----
+
+    Dog spot;
+    string name {"Spot"};
+    spot.set_name(name);
+    spot.set_age(5);
+    cout << spot.get_name() << endl;
+    cout << spot.get_age() << endl;
+    cout << spot.get_human_years() << endl;
+    cout << spot.speak() << endl;
+
+//---- WRITE YOUR CODE ABOVE THIS LINE----
+//---- DO NOT CHANGE THE CODE BELOW----
+    return spot;
+}
+
+int main() {
+    test_dog();
+    return 0;
+};
